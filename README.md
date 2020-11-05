30224ceeeaf494b480982d4819fdc670f65ed7ce
louibrena02 <johnlouibrena27@gmail.com>
2020-11-05 21:31:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-wt245q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-wt245q)
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-profile",
+  template: "./profile.component.html",
+
+})
+export class ProfileComponent {
+  
+}
